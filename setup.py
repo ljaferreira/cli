10530d48ee95ec6272c375b829327e17bc87f0a2
Leandro Ferreira <ljaferreira@gmail.com>
2024-02-27 17:44:22 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ setup(
     name='sula',
     version='1.0.0',
     packages=find_packages(),
-    py_modules=['sula'],
+    py_modules=['main', 'utils', 'windows', 'linux'],
     install_requires=[
-        'Click',
+        'click == 8.1.7',
+        'google-cloud-workstations == 0.5.4',
+        'psutil == 5.9.8',
+        'google-auth == 2.28.1',
+        'setuptools == 69.1.1',
+        'regex == 2023.12.25'
     ],
     entry_points={
         'console_scripts': [
